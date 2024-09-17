becc3ff8ae7f631c0971f481888a84ff923117fe
YongIn Kim <limitkr99@gmail.com>
2024-09-18 01:26:15 +0900

    Create starter_codeforces.cpp

---
+++ template/starter_codeforces.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file $filename$
+ * @author limitkr
+ * @version 0.1
+ * @date $date$
+ */
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+#define FAST_IO cin.tie(nullptr)->sync_with_stdio(false)
+#define endl '\n'
+#define vt vector
+#define ar array
+
+int N;
+
+void solve() {}
+
+auto main() -> int32_t {
+  FAST_IO;
+  cin >> N;
+  solve();
+  return 0;
+}
