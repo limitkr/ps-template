059d4232093e787df032afbd3769dcca251c0036
YongIn Kim <limitkr99@gmail.com>
2024-10-09 18:55:24 +0900

    Create ordered_set.cpp

---
+++ template/__gnu_pbds/ordered_set.cpp
@@ -0,0 +1,8 @@
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
