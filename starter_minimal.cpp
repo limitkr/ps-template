5f28846637cde5dd0131396d0f5d9668f3ee438c
YongIn Kim <limitkr99@gmail.com>
2025-01-11 16:17:57 +0900

    Create starter_minimal.cpp

---
+++ starter_minimal.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file $filename$
+ * @author limitkr
+ * @version 0.1
+ * @date $date$
+ */
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/extc++.h>
+using namespace std;
+
+#define FAST_IO cin.tie(nullptr)->sync_with_stdio(false)
+
+auto main() -> int32_t {
+  FAST_IO;
+  /** Write code here */
+  return 0;
+}
