0335e87977fa06e8b1dd5a3f7ed4207bc90d7aab
YongIn Kim <limitkr99@gmail.com>
2025-01-11 16:16:25 +0900

    Create CP_TEMPLATE_CPP.cpp

---
+++ CP_TEMPLATE_CPP.cpp
@@ -0,0 +1,17 @@
+/**
+ * ${FILE_NAME}
+ * @author limitkr
+ * @date ${DATE} ${HOUR}:${MINUTE}:${SECOND}
+ */
+#[[#pragma]]# GCC optimize("O3,unroll-loops")
+#[[#include]]# <bits/extc++.h>
+using namespace std;
+
+#[[#define]]# FAST_IO cin.tie(nullptr)->sync_with_stdio(false)
+
+auto main() -> int32_t {
+  FAST_IO;
+  /** Write code here */
+  $END
+  return 0;
+}
