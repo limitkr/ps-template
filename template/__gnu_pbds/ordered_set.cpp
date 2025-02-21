696501313887b5544cd15498bc8119d41c1be099
YongIn Kim <limitkr99@gmail.com>
2025-02-21 15:10:49 +0900

    Update ordered_set.cpp

--- template/__gnu_pbds/ordered_set.cpp
+++ template/__gnu_pbds/ordered_set.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-typedef tree<int, null_type, less<>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    ordered_set;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
