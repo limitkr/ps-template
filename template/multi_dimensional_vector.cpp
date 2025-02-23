a112feb245c86bd9a05bb8b1c4df9ad23add1195
YongIn Kim <limitkr99@gmail.com>
2025-02-23 10:32:43 +0900

    Update multi_dimensional_vector.cpp

--- template/multi_dimensional_vector.cpp
+++ template/multi_dimensional_vector.cpp
@@ -1,8 +1,10 @@
-template <int N, typename T> struct Vec : vector<Vec<N - 1, T>> {
+template <int N, typename T>
+struct Vec : vector<Vec<N - 1, T>> {
   template <typename... Args>
   explicit Vec(int n = 0, Args... args)
       : vector<Vec<N - 1, T>>(n, Vec<N - 1, T>(args...)) {}
 };
-template <typename T> struct Vec<1, T> : vector<T> {
-  explicit Vec(int n = 0, const T &val = T()) : vector<T>(n, val) {}
+template <typename T>
+struct Vec<1, T> : vector<T> {
+  explicit Vec(int n = 0, const T& val = T()) : vector<T>(n, val) {}
 };
